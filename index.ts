e9920a02f660c99b2578037ce3e3df70afc3ddc9
Jacek Rojek <j.rojek19@gmail.com>
2019-03-04 00:58:39 +0000

    linear function added

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import * as d3 from 'd3';
-import { onRefresh, onTrain, onTest, onNoiseChange, onSlopeChange } from './ui';
+import { onRefresh, onTrain, onTest, onNoiseChange, onSlopeChange, onLearningRateChange, onOffsetChange } from './ui';
 
 const Y_MAX = 400;
 const X_MAX = 400;
 let noiseLevel = 1;
-let slope = 1;
+let slope = 2;
+let learningRate = 0.2;
+let offset = 0;
 
 const rand = (high, low) => Math.random() * (high - low) + low;
 
@@ -12,44 +14,52 @@ type Team = 1 | -1;
 type Weights = {
   x: number;
   y: number;
+  bias: number;
 };
 type Point = {
   x: number;
   y: number;
+  bias: number;
 };
+
+const f = (x) => (slope * x) + offset;
+
+const activationFunction = (sum:number):Team => sum >= 0 ? 1 : -1;
 
 const guess = (weights:Weights, point:Point):Team => {
   const sum = point.x * weights.x
-            + point.y * weights.y;
-  const team = sum >= 0 ? 1 : -1;
+            + point.y * weights.y
+            + point.bias * weights.bias;
+
+  const team = activationFunction(sum);
   return team;
 };
 
 function train(weights:Weights, point:Point, team:Team):Weights {
   const guessResult = guess(weights, point);
   const error = team - guessResult;
-  const learningRate = 0.5;
   return {
     x: weights.x + point.x * error * learningRate,
     y: weights.y + point.y * error * learningRate,
+    bias: weights.bias + point.bias * error * learningRate,
   };
 }
 
 const randomWeights = ():Weights => ({
   x: rand(-1, 1),
   y: rand(-1, 1),
+  bias: rand(-1, 1),
 });
 
-const team = (point:Point):Team => (
-  point.x + rand(-noiseLevel, noiseLevel) >
-  slope * point.y + rand(-noiseLevel, noiseLevel)
-  ? 1
-  : -1
-);
+const label = (point:Point):Team => {
+  return f(point.x) > point.y ? 1 : -1
+};
 
-const generatePoints = (num:number):Point[] => Array.from(Array(num)).map(() => ({
-  x: rand(0, X_MAX),
-  y: rand(0, Y_MAX),
+const generatePoints = (num:number):Point[] => Array
+.from(Array(num)).map(() => ({
+  x: rand(-X_MAX/2, X_MAX/2),
+  y: rand(-Y_MAX/2, Y_MAX/2),
+  bias: 1,
 }));
 
 const randomPoints = generatePoints(200);
@@ -61,57 +71,55 @@ const draw = (weights:Weights, pointsDataset: Point[]) => {
   .append("svg")
   .attr("width", X_MAX)
   .attr("height", Y_MAX)
-  const padding = 20;
   const points = svg.selectAll("circle")
     .data(pointsDataset)
     .enter()
     .append("circle")
-    .attr("cx", p => p.x)
-    .attr("cy", p => Y_MAX - p.y)
+    .attr("cx", p => p.x + X_MAX/2)
+    .attr("cy", p => Y_MAX/2 - p.y)
     .attr("r", 2)
     .style("fill", (p) => guess(weights, p) === -1  ? 'red' : 'blue');
 
     var line = svg
       .append("line")
-      .attr("x1", 0)
-      .attr("y1", Y_MAX)
-      .attr("x2", X_MAX * slope)
-      .attr("y2", 0)
+      .attr("x1", X_MAX)
+      .attr("y1", Y_MAX/2 - f(X_MAX/2))
+      .attr("x2", 0)
+      .attr("y2", Y_MAX/2 - f(-X_MAX/2))
       .attr("stroke-width", 0.5)
       .attr("stroke", "green")
 
       var y_axis = d3.axisLeft(
-        d3.scaleLinear().range([Y_MAX, 0]))
+        d3.scaleLinear().range([Y_MAX/2, -Y_MAX/2]))
       var x_axis = d3.axisBottom(
-        d3.scaleLinear().range([0, X_MAX]))
+        d3.scaleLinear().range([-X_MAX/2, X_MAX/2]))
 
-      svg.append("g")
-        .attr("transform", "translate(0, 0)")
-        .call(y_axis);
-      svg.append("g")
-        .attr("transform", `translate(0, ${Y_MAX})`)
+        svg.append("g")
+        .attr("transform", `translate(${X_MAX/2}, ${Y_MAX})`)
         .call(x_axis);
+        svg.append("g")
+          .attr("transform", `translate(0, ${X_MAX/2})`)
+          .call(y_axis);
 };
 
 let currentWeights = randomWeights();
 const updateWeights = (trainingSetSize: number, initialWeights: Weights) => {
   const examples = generatePoints(trainingSetSize).map(point => ({
     point,
-    team: team(point),
+    label: label(point),
   }));
   
   currentWeights = initialWeights;
   examples.forEach((example) => {
-    currentWeights = train(currentWeights, example.point, example.team);
+    currentWeights = train(currentWeights, example.point, example.label);
     return currentWeights;
   });
-  console.log('currentWeights::', currentWeights)
   draw(currentWeights, randomPoints);
   // drawing test data
   // draw(currentWeights, examples.map(e => e.point));
 };
 
-updateWeights(100, randomWeights());
+updateWeights(100000, randomWeights());
 
 // UI binding
 
@@ -120,3 +128,5 @@ onTrain((setSize) => updateWeights(setSize, currentWeights))
 onTest((points) => draw(currentWeights, generatePoints(points)))
 onNoiseChange((newValue) => { noiseLevel = newValue})
 onSlopeChange((newValue) => { slope = newValue})
+onLearningRateChange((newValue) => { learningRate = newValue})
+onOffsetChange((newValue) => { offset = newValue})
--- ui.ts
+++ ui.ts
@@ -5,13 +5,23 @@ const trainSetSize = document.getElementById('training-set-size') as HTMLInputEl
 const testSize = document.getElementById('test-size') as HTMLInputElement;
 const noise = document.getElementById('noise-level') as HTMLInputElement;
 const slope = document.getElementById('slope') as HTMLInputElement;
+const offset = document.getElementById('offset') as HTMLInputElement;
+const learningRate = document.getElementById('learning-rate') as HTMLInputElement;
 
 export const onRefresh = (callback) => refresh.onclick = () => {
   callback(Number(trainSetSize.value));
 }
 
+export const onLearningRateChange = (callback) => learningRate.onclick = () => {
+  callback(parseFloat(learningRate.value));
+}
+
 export const onSlopeChange = (callback) => slope.onchange = () => {
   callback(parseFloat(slope.value));
+}
+
+export const onOffsetChange = (callback) => offset.onchange = () => {
+  callback(parseFloat(offset.value));
 }
 
 export const onNoiseChange = (callback) => noise.onchange = () => {
